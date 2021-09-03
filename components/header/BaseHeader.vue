65b1bb0c62a0e49ed53729d23a017121b2a1e6a1
Paul Anthony McGowan <xhostcom@gmail.com>
2021-09-03 09:24:54 +0100

    First Commit

---
+++ components/header/BaseHeader.vue
@@ -0,0 +1,129 @@
+<template>
+<header id="site-head">
+			<div class="uk-container uk-container-expand">
+				<div class="uk-grid uk-grid-medium uk-flex uk-flex-middle uk-flex-between" data-uk-grid>
+					<div class="uk-width-auto" id="logo">
+						<a href="#" class="uk-logo" title="eCommerce"><img src="logo.svg" alt="Logo"></a>
+					</div>
+                     <div class="uk-width-auto">
+					    <ul class="uk-subnav" data-uk-margin>
+                         <li class="uk-visible@s"><NuxtLink to="/">Home</NuxtLink></li>
+						  	  <li class="uk-visible@s"><NuxtLink to="/">About</NuxtLink></li>
+							<li class="uk-visible@s"><NuxtLink to="/contact">Collections</NuxtLink></li>
+								  <li class="uk-visible@s"><NuxtLink to="/">Categories</NuxtLink></li>
+							<li class="uk-visible@s"><NuxtLink to="/contact">Best Sellers</NuxtLink></li>
+									  <li class="uk-visible@s"><NuxtLink to="/">Instagram</NuxtLink></li>
+							<li class="uk-visible@s"><NuxtLink to="/contact">Trending</NuxtLink></li>
+								  <li class="uk-visible@s"><NuxtLink to="/">Sale</NuxtLink></li>
+							<li class="uk-visible@s"><NuxtLink to="/contact">New</NuxtLink></li>
+								<li class="uk-visible@s"><NuxtLink to="/contact">Journal</NuxtLink></li>
+							<li class="uk-visible@s"><NuxtLink to="/contact">Contact</NuxtLink></li>
+                           <li class="uk-visible@s"> <VmSearch></VmSearch></li>
+				
+							
+		       
+								<li class="uk-visible@s">
+								<a href="#" data-uk-icon="icon:cart"></a>
+								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
+									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Shopping Cart</h4>
+								    <hr>
+<ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
+									
+										
+										<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
+									</ul>
+                                    <hr>
+									<a href="#" class="uk-button uk-button-secondary uk-width-1-1">CHECKOUT</a>
+									<hr>
+									
+								</div>
+							</li>
+							<li class="uk-visible@s">
+								<a href="#" data-uk-icon="icon:heart"></a>
+								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
+									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Wishlist</h4>
+									<hr>
+<ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
+								
+									<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
+									</ul>
+                                    <hr>
+									<a href="#" class="uk-button uk-button-secondary uk-width-1-1">ADD ALL TO CART</a>
+									<hr>
+									
+								</div>
+							</li>
+						
+									<li class="uk-visible@s">
+								<a href="#" data-uk-icon="icon:user"></a>
+								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
+									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Account</h4>
+								    <hr>
+<ul class="uk-list uk-list-space">
+									 <li><span class="uk-margin-small-right" data-uk-icon="icon: user"></span>User Profile</li>
+									  <li><span class="uk-margin-small-right" data-uk-icon="icon: cog"></span>Account</li>
+									   <li><span class="uk-margin-small-right" data-uk-icon="icon: mail"></span>Messsages</li>
+									    <li><span class="uk-margin-small-right" data-uk-icon="icon: sign-out"></span>Logout</li>
+									</ul>
+                               
+									<hr>
+									
+								</div>
+							</li>
+						<li class="uk-hidden@s"><a data-uk-navbar-toggle-icon data-uk-toggle href="#offcanvas-nav"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+                 <OffCanvas />
+		</header>
+   
+     <!--/HEADER-->
+</template>
+<script>
+import VmSearch from '../search/Search';
+import OffCanvas from '@/components/header/OffCanvas';
+
+
+export default {
+name: 'Header',
+ data () {
+      return {
+       isCheckoutActive: false,
+       isMenuOpen: false
+      }
+    },
+components: {
+      VmSearch, 
+      OffCanvas
+},
+  computed: {
+      numProductsAdded () {
+        return this.$store.getters.productsAdded.length;
+      }
+    },
+
+    methods: {
+      showCheckoutModal () {
+        this.$store.commit('showCheckoutModal', true);
+      }
+   }
+}
+</script>
+
+<style scoped>
+#site-head {
+padding-right: 50px;
+padding-left: 30px;
+}
+#logo {
+padding-bottom: 6px;
+}
+h4.uk-margin-small-bottom {
+text-transform: uppercase;
+font-size: 1em;
+}
+li.uk-visible@s a {
+padding-right: 20px;
+}
+</style>
---
+++ components/header/OffCanvas.vue
@@ -0,0 +1,36 @@
+<template>
+ <!-- OFFCANVAS -->
+		<div id="offcanvas-nav" data-uk-offcanvas="flip: true; overlay: true">
+			<div class="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide">
+				<button class="uk-offcanvas-close uk-close uk-icon" type="button" data-uk-close></button>
+			    <ul class="uk-nav uk-nav-offcanvas uk-nav-parent-icon" data-uk-nav="{ multiple: true }">
+					<li class="uk-nav-header">Menu</li>
+					<li><a href="#js-options"><span class="uk-margin-small-right uk-icon" data-uk-icon="icon: thumbnails"></span> Item</a></li>
+			   </ul>
+				<h4>Information</h4>
+				<p>eCommerce using UIKit 3, Nuxt Js and Vue JS, with Off Canvas and other UIKit components.</p>
+				<VmSearch></VmSearch>
+                		<h4>Cart</h4>
+                        <ul class="uk-nav uk-nav-offcanvas uk-nav-parent-icon" data-uk-nav="{ multiple: true }">
+								
+						<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
+						</ul>
+                        <hr>
+						<a href="#" class="uk-button uk-button-secondary uk-width-1-1">CHECKOUT</a>
+			 <NuxtLink to="/login" id="login" class="uk-button uk-button-primary uk-border-rounded uk-align-center">Login</NuxtLink>
+			</div>
+		</div>
+		<!-- /OFFCANVAS -->
+</template>
+<script>
+import VmSearch from '../search/Search';
+export default {
+name: 'OffCanvas',
+components: {
+      VmSearch
+},    
+}
+</script>
+
+<style scoped>
+</style>
