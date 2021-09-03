0778f7da73bf624c0e9e4ff235664059e622b0d9
Paul Anthony McGowan <xhostcom@gmail.com>
2021-09-03 22:09:21 +0100

    Menu

--- components/header/BaseHeader.vue
+++ components/header/BaseHeader.vue
@@ -19,53 +19,50 @@
 								<li class="uk-visible@s"><NuxtLink to="/contact">Journal</NuxtLink></li>
 							<li class="uk-visible@s"><NuxtLink to="/contact">Contact</NuxtLink></li>
                            <li class="uk-visible@s"> <VmSearch></VmSearch></li>
-				
-							
-		       
-								<li class="uk-visible@s">
+				            <li class="uk-visible@s">
 								<a href="#" data-uk-icon="icon:cart"></a>
 								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
 									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Shopping Cart</h4>
 								    <hr>
-<ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
-									
-										
-										<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
+                                     <ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
+									<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
 									</ul>
                                     <hr>
 									<a href="#" class="uk-button uk-button-secondary uk-width-1-1">CHECKOUT</a>
 									<hr>
-									
+									</div>
+							     </li>
+	                        	<li class="uk-visible@s">
+								<a href="#" data-uk-icon="icon:user"></a>
+								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
+									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Account</h4>
+								    <hr>
+                                    <ul v-if="isUserLoggedIn" class="uk-list uk-list-space">
+									 <li><span class="uk-margin-small-right" data-uk-icon="icon: user"></span>Welcome {{ getUserName }}</li>
+								   
+									  <li><span class="uk-margin-small-right" data-uk-icon="icon: cog"></span>Account</li>
+									   <li><span class="uk-margin-small-right" data-uk-icon="icon: mail"></span>Messsages</li>
+									  <li ><a href="#" class="uk-button uk-button-secondary uk-width-1-1">Log Out</a></li>
+									 </ul>
+									  <ul v-else class="uk-list uk-list-space">
+									  <li ><a href="#" class="uk-button uk-button-secondary uk-width-1-1">Sign Up</a></li>
+                                     </ul>
+									<hr>
 								</div>
 							</li>
 							<li class="uk-visible@s">
 								<a href="#" data-uk-icon="icon:heart"></a>
 								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
-									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Wishlist</h4>
+									<NuxtLink :to="{ name: 'user-wishlist' }"><h4 class="uk-margin-small-bottom uk-margin-remove-adjacent"></h4>
+											{{ wishlistLabel }}
+				                    </NuxtLink>
 									<hr>
-<ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
+                                    <ul class="uk-nav-default uk-nav-parent-icon" data-uk-nav>
 								
 									<li><img src="purse.jpg" width="60" /><span class="uk-margin-small-left"></span>Mens Purse £240<span class="uk-margin-small-left" data-uk-icon="icon: trash"></span><span class="uk-margin-small-left" data-uk-icon="icon: pencil"></span></li>
 									</ul>
                                     <hr>
 									<a href="#" class="uk-button uk-button-secondary uk-width-1-1">ADD ALL TO CART</a>
-									<hr>
-									
-								</div>
-							</li>
-						
-									<li class="uk-visible@s">
-								<a href="#" data-uk-icon="icon:user"></a>
-								<div class="drop-nav uk-dropdown" data-uk-dropdown="mode: click; offset: 20;animation: uk-animation-slide-bottom-small; duration: 150">
-									<h4 class="uk-margin-small-bottom uk-margin-remove-adjacent">Account</h4>
-								    <hr>
-<ul class="uk-list uk-list-space">
-									 <li><span class="uk-margin-small-right" data-uk-icon="icon: user"></span>User Profile</li>
-									  <li><span class="uk-margin-small-right" data-uk-icon="icon: cog"></span>Account</li>
-									   <li><span class="uk-margin-small-right" data-uk-icon="icon: mail"></span>Messsages</li>
-									    <li><span class="uk-margin-small-right" data-uk-icon="icon: sign-out"></span>Logout</li>
-									</ul>
-                               
 									<hr>
 									
 								</div>
