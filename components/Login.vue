4c71b2aa46aced8758d21d18ebfee5802d7cf990
Paul Anthony McGowan <xhostcom@gmail.com>
2021-09-03 23:02:02 +0100

    Forms

--- components/Login.vue
+++ components/Login.vue
@@ -33,10 +33,77 @@
 	</div>
 </div>
 </template>
-
 <script>
+import { isValidEmail } from '@/assets/validators';
 export default {
+ name: 'login',
 
+  data () {
+    return {
+      primaryBtnLabel: 'Log in',
+      emailRequiredLabel: 'Email required',
+      passwordRequiredLabel: 'Password required',
+      emailNotValidLabel: 'Valid email required',
+      btnLoggedInLabel: 'Close',
+      emailPlaceholder: 'Your email',
+      email: '',
+      password: '',
+      highlightEmailWithError: null,
+      highlightPasswordWithError: null,
+      isFormSuccess: false
+    };
+  },
+    computed: {
+    isUserLoggedIn () {
+      return this.$store.getters.isUserLoggedIn;
+    },
+ },
+    methods: {
+    checkForm (e) {
+      e.preventDefault();
+
+      if (this.email && this.password) {
+        this.highlightEmailWithError = false;
+        this.highlightPasswordWithError = false;
+        this.isFormSuccess = true;
+        this.$store.commit('isUserLoggedIn', this.isFormSuccess);
+      }
+
+      if (!this.email) {
+        this.highlightEmailWithError = true;
+
+        if (this.email && !isValidEmail(this.email)) {
+          this.emailRequiredLabel = this.emailNotValidLabel;
+        }
+      } else {
+        this.highlightEmailWithError = false;
+      }
+
+      if (!this.password) {
+        this.highlightPasswordWithError = true;
+      } else {
+        this.highlightPasswordWithError = false;
+      }
+    },
+    checkEmailOnKeyUp (emailValue) {
+      if (emailValue && isValidEmail(emailValue)) {
+        this.highlightEmailWithError = false;
+      } else {
+        this.highlightEmailWithError = true;
+
+        if (!isValidEmail(emailValue)) {
+          this.emailRequiredLabel = this.emailNotValidLabel;
+        }
+      }
+    },
+    checkPasswordOnKeyUp (passwordValue) {
+      if (passwordValue) {
+        this.highlightPasswordWithError = false;
+      } else {
+        this.highlightPasswordWithError = true;
+      }
+    }
+  }
 }
 </script>
 
