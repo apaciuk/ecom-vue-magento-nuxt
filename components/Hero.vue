2865965e8f987fd8fef8fcd81f8f709f681068e1
Paul Anthony McGowan <xhostcom@gmail.com>
2021-09-04 12:31:09 +0100

    Contact Page

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="hero is-medium is-bold">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Free ecommerce template for Vue.js projects
+  <div class="section">
+      <div class="uk-container">
+        <h1 class="uk-title">
+          UIKit Nuxt ecommerce template for Vue.js projects
         </h1>
-        <h2 class="subtitle">
+        <h2 class="uk-subtitle">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
         </h2>
       </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
     .hero {
-      background: rgb(81, 186, 252);
+      background: rgb(201, 16, 185);
     }
     h1, h2 {
       color: #fff;
